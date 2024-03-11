c191c751d5175e02012e24bb4a7e36d9cbe840dd
Christina Kiesz <ckiesz@gmail.com>
2024-03-10 22:52:36 -0700

    nicer cards

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,22 +1,10 @@
-import { getRecipeCategoriesTitles } from "../lib/data";
 import { recipes_data } from "../../../cypress/fixtures/recipes_data";
 
-// data comes in in an array of objects, each object is an joined result of category and recipe
-const reshapeData = (data: any) => {
-	const newData: any = {};
-	data.forEach((recipe: any) => {
-		newData[recipe.category] = {
-
-		}
-	});
-	return newData;
-}
-
-const RecipeList = ({category, categoryRecipes}: {category: string, categoryRecipes: any}) => {
-	
+const RecipeList = ({ category, categoryRecipes }: { category: string, categoryRecipes: any }) => {
+	const formattedCategory = category.charAt(0).toUpperCase() + category.slice(1);
 	return (
 		<div className="m-4 border-2 rounded-md p-2">
-			<h2 className="m-1">{category}</h2>
+			<h2 className="font-bold">{formattedCategory}</h2>
 			<ul>
 				{categoryRecipes.map((recipe: any, index: number) => {
 					return (
@@ -31,24 +19,18 @@ const RecipeList = ({category, categoryRecipes}: {category: string, categoryReci
 
 // Recipe Index Page should display a list of recipes
 export default async function Page() {
-	const dataRows = await getRecipeCategoriesTitles();
-	const catObject = reshapeData(dataRows);
-	console.log(catObject);
 	return (
 		<div>
 			<title>Recipes</title>
 			<main className='flex flex-col items-center'>
 				<h1 className='m-4' data-cy='recipes-header'>Recipes</h1>
-				<div className='' data-cy='recipe-categories'>
-					<h2>By Category:</h2>
-					<div className="m-2 flex flex-row">
-						{Object.keys(recipes_data).map((catName, index) => {
-							const catGroup: any = recipes_data[catName];
-							return (
-								<RecipeList key={index} category={catName} categoryRecipes={catGroup} />	
-							);
-						})}
-					</div>
+				<div data-cy='recipe-categories' className="m-2 flex flex-row">
+					{Object.keys(recipes_data).map((catName, index) => {
+						const catGroup: any = recipes_data[catName];
+						return (
+							<RecipeList key={index} category={catName} categoryRecipes={catGroup} />
+						);
+					})}
 				</div>
 			</main>
 		</div>
