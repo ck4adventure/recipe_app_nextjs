d4c712af3f601253961644da281f58647ed9c0f1
Christina Kiesz <ckiesz@gmail.com>
2025-02-25 15:25:19 -0800

    better styling for recipe page

--- src/app/chef/recipes/[slug]/page.tsx
+++ src/app/chef/recipes/[slug]/page.tsx
@@ -5,22 +5,29 @@ import { GET_CHEFS_RECIPE_BY_SLUG } from "@/app/_lib/sqlQueriesChef";
 export default async function Page({ params }: { params: { slug: string } }) {
 	// fetch ingr and list all properties
 	const recipe = await GET_CHEFS_RECIPE_BY_SLUG(params.slug);
-	console.log(recipe)
 	return (
-		<div className='flex flex-col justify-center m-4'>
-			<p>{recipe.title}</p>
-			<ul>
-				{recipe.ingredients && recipe.ingredients.map((ingr: any) => (
-					<li key={ingr.package_name}>{ingr.qty} {ingr.measure} {ingr.brand} {ingr.packaged_name}</li>
-				))}
-			</ul>
-			<ul>
-				{recipe.steps && recipe.steps.map((step: any, i: number) => (
-					<li key={i}>
-						{step}
-					</li>
-				))}
-			</ul>
+		<div className='flex flex-col items-center'>
+			<h2 className="font-bold text-lg text-center">{recipe.title}</h2>
+			<div className="max-w-[650px]">
+				<div className="m-4">
+				<h3 className="font-semibold my-2">Ingredients</h3>
+				<ul className="">
+					{recipe.ingredients && recipe.ingredients.map((ingr: any) => (
+						<li key={ingr.package_name}>{ingr.qty} {ingr.measure} {ingr.brand_name} {ingr.packaged_name}</li>
+					))}
+				</ul>
+				</div>
+				<div className="m-4">
+				<h3 className="font-semibold my-2">Method</h3>
+				<ol className="list-decimal">
+					{recipe.steps && recipe.steps.map((step: any, i: number) => (
+						<li key={i}>
+							{step}
+						</li>
+					))}
+				</ol>
+				</div>
+			</div>
 		</div>
 	);
 }
