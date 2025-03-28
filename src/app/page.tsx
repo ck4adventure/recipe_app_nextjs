1ef9e523ced116246daa9b10c4df54e32660dc27
Christina Kiesz <ckiesz@gmail.com>
2025-03-28 16:59:44 -0700

    update home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,28 @@
 import * as React from 'react';
-import { Link, Typography, Box, Paper, Button } from "@mui/material";
-import Grid from '@mui/material/Grid2';
-import { NewDoughButton, NewLeavenButton } from './_ui/loafer/components';
-import { StandardModal } from './_ui/loafer/modal_standard';
-import { ST } from 'next/dist/shared/lib/utils';
-import { StatusSection } from './_ui/loafer/main_status_section';
-
-const AppSections = [
-	["Leavens", "/loafer/leaven"],
-	["Doughs", "loafer/dough"],
-	["Loaves and Bakes", "loafer/loaf"],
-	["Products", "/"],
-	["Recipes", "/recipes"],
-	["Ingredients", "/"],
-]
-
-// TODO: finish button with a modal that opens with a set of links to available /new routes
-
+import Link from 'next/link';
 
 
 export default async function Main() {
 
 	return (
-		<main className="min-h-screen">
-			<StatusSection />
-			<Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }} margin={{ xs: 2, md: 4 }}>
-
-				{/* {AppSections.map((entry, index) => (
-					<Grid key={index} size={{ xs: 4, sm: 4, md: 6 }}>
-						<Paper elevation={2}>
-							<Box sx={{ minHeight: 75, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-								<Typography><Link href={entry[1]} underline="none">{entry[0]}</Link></Typography>
-							</Box>
-						</Paper>
-					</Grid>
-				))} */}
-			</Grid>
-
-
-			<Typography variant="h5"></Typography>
+		<main className="min-h-screen flex flex-col items-center">
+			<div className='w-[1000px] border-8 border-slate-200 m-4 p-4'>
+			<Link href={"/chef"} className='font-bold'>Chef</Link>
+			<div className='mx-4 flex'>
+				<Link href={"/chef/products"} className='m-2 text-xl'>Products</Link>
+				<Link href={"/chef/recipes"} className='m-2 text-xl'>Recipes</Link>
+				<Link href={"/chef/ingrs"}  className='m-2 text-xl'>Ingredients</Link>
+			</div>
+			</div>
+			<div className='w-[1000px] border-8 border-slate-200 m-4 p-4'>
+			<Link href={"/recipes"} className='font-bold'>Home Cooking</Link>
+			<div className='mx-4 flex'>
+				<Link href={"/recipes"} className='m-2 text-xl'>Recipes</Link>
+				<Link href={"/authors"} className='m-2 text-xl'>Authors</Link>
+				<Link href={"/sources"} className='m-2 text-xl'>Sources</Link>
+				<Link href={"/categories"} className='m-2 text-xl'>Categories</Link>
+			</div>
+			</div>
 		</main>
 	);
 }
