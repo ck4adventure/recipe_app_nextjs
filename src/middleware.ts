476a46e23f91eaadf2ccb9eeac06813afbfe5a89
Christina Kiesz <ckiesz@gmail.com>
2025-06-10 18:43:52 -0700

    put recipes/create behind auth route

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,5 +24,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/chef/:path*', '/loafer/:path*'],
+  matcher: ['/chef/:path*', '/loafer/:path*', '/recipes/create'],
 };
