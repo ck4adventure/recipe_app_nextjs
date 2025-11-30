45db3d248fd2694f2bb9ac788a25818d56baa716
Christina Kiesz <ckiesz@gmail.com>
2025-11-30 16:00:04 -0700

    better revalidation on category index page

--- src/app/blue-binder/categories/[slug]/page.tsx
+++ src/app/blue-binder/categories/[slug]/page.tsx
@@ -1,4 +1,7 @@
 import { GET_RECIPES_FOR_CATEGORY_SLUG } from "@/app/_lib/sqlQueriesRecipes";
+
+export const revalidate = 60; // revalidate in seconds
+
 // import AddRecipeButton from "@/app/_ui/recipes/add_recipe_button";
 import RecipesListView from "@/app/_ui/recipes/recipe_list_view";
 export default async function Page({ params }: {params: {slug: string}}) {
